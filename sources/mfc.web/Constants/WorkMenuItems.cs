33dd550accad2ef7edf00cddd4cf77faa229c7be
Oleg Agafonov <oagafonov@mail.ru>
2015-05-29 00:08:28 +0500

    изменена работа с пунктами рабочего меню - идентификация по стоковой константе

---
+++ sources/mfc.web/Constants/WorkMenuItems.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mfc.web.Constants {
+    public class WorkMenuItems {
+        public const string Services = "Services";
+        public const string Files = "Files";
+        public const string Reports = "Reports";
+    }
+}
