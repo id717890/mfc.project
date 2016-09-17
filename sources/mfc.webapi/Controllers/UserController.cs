f6fa5687a8d0061f3c83dce9a92c7c8075592ec3
oagafonov <oleg.a.agafonov@gmail.com>
2016-09-17 12:48:48 +0500

    реализованы get запросы для справочника пользователей

---
+++ sources/mfc.webapi/Controllers/UserController.cs
@@ -0,0 +1,64 @@
+﻿using mfc.domain.services;
+using mfc.webapi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace mfc.webapi.Controllers
+{
+    public class UserController : ApiController
+    {
+        // GET: api/User
+        public HttpResponseMessage Get()
+        {
+            var user_service = CompositionRoot.Resolve<IUserService>();
+            var users = new List<AccountInfo>();
+
+            foreach (var user in user_service.GetAllUsers())
+            {
+                users.Add(new AccountInfo
+                {
+                    Id = user.Id,
+                    UserName = user.Account,
+                    Description = user.Name
+                });
+            }
+            
+            return Request.CreateResponse(HttpStatusCode.OK, users);
+        }
+
+        // GET: api/User/5
+        public HttpResponseMessage Get(int id)
+        {
+            var user_service = CompositionRoot.Resolve<IUserService>();
+            var user = user_service.GetUserById(id);
+
+            if (user != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.Found, new AccountInfo(user));
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+        }
+
+        // POST: api/User
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/User/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/User/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ sources/mfc.webapi/Models/AccountInfo.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace mfc.webapi.Models {
+    [JsonObject("account_info")]
+    public class AccountInfo {
+        public AccountInfo() { }
+
+        public AccountInfo(mfc.domain.entities.User user)
+        {
+            Id = user.Id;
+            UserName = user.Account;
+            Description = user.Name;
+            IsAdmin = user.IsAdmin;
+            IsController = user.IsController;
+            IsExpert = user.IsExpert;
+        }
+        
+        [JsonProperty("id")]
+        public Int64 Id { get; set; }
+
+        [Required]
+        [Display(Name = "Имя пользователя")]
+        [JsonProperty("user_name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Ф.И.О.")]
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("is_admin")]
+        public bool IsAdmin { get; set; }
+        [JsonProperty("is_expert")]
+        public bool IsExpert { get; set; }
+        [JsonProperty("is_controller")]
+        public bool IsController { get; set; }
+
+        public mfc.domain.entities.User ConvertToUser(AccountInfo account)
+        {
+            return new mfc.domain.entities.User
+            {
+                Id = account.Id,
+                Account = account.UserName,
+                Name = account.Description,
+                IsAdmin = account.IsAdmin,
+                IsExpert = account.IsExpert,
+                IsController = account.IsController
+            };
+        }
+    }
+}   
