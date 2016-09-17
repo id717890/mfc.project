6eb743415a701a6886a37005a391612152bdf915
oagafonov <oleg.a.agafonov@gmail.com>
2016-09-17 11:26:46 +0500

    добавлен код для инициацилации инфраструктуры

--- sources/mfc.webapi/Global.asax.cs
+++ sources/mfc.webapi/Global.asax.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using mfc.infrastructure.services;
+using mfc.web;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Security;
 
 namespace mfc.webapi
 {
@@ -18,6 +23,38 @@ namespace mfc.webapi
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //Настройка параметров подключения к серверу СУБД
+            string conntion_string_name = ConfigurationManager.AppSettings["ConnectionStringName"];
+            string connection_string = ConfigurationManager.ConnectionStrings[conntion_string_name].ConnectionString;
+            CompositionRoot.Resolve<ISqlProvider>().Init(connection_string);
+
+            log4net.Config.XmlConfigurator.Configure();
+        }
+
+        protected void Application_AuthenticateRequest(Object sender, EventArgs e)
+        {
+            HttpCookie authCookie = Context.Request.Cookies[FormsAuthentication.FormsCookieName];
+            if (authCookie == null || authCookie.Value == "")
+                return;
+
+            FormsAuthenticationTicket authTicket;
+            try
+            {
+                authTicket = FormsAuthentication.Decrypt(authCookie.Value);
+            }
+            catch
+            {
+                return;
+            }
+
+            // retrieve roles from UserData
+            string[] roles = authTicket.UserData.Split(';');
+
+            if (Context.User != null)
+            {
+                Context.User = new GenericPrincipal(Context.User.Identity, roles);
+            }
         }
     }
 }
