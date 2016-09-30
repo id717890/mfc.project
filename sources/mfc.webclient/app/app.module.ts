5bdd2517a756a9a35d943c34266451aa147fb767
Zamir Yusupov <jusupovz@gmail.com>
2016-09-30 18:29:27 +0500

    добавил модуль workspace для работы

--- sources/mfc.webclient/app/app.module.ts
+++ sources/mfc.webclient/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
 import { MenuComponent }   from './menu/menu.component';
+import { WorkspaceComponent} from './workspace/workspace.component';
+
 @NgModule({
     imports: [BrowserModule],
-    declarations: [AppComponent, MenuComponent],
+    declarations: [AppComponent, MenuComponent, WorkspaceComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ sources/mfc.webclient/app/workspace/workspace.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'mfc-workplace',
+    templateUrl: 'app/workspace/workspace.component.html'
+})
+export class WorkspaceComponent {
+    
+}
