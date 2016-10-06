e0270455cbbbbb6980ee785b75d320ebc5ef3100
oagafonov <oleg.a.agafonov@gmail.com>
2016-10-06 08:04:42 +0500

    1. реализовано компонент списка 2. реализована операция удаления

--- sources/mfc.webclient/app/admin/customer-types/customer-type-list.component.ts
+++ sources/mfc.webclient/app/admin/customer-types/customer-type-list.component.ts
@@ -20,6 +20,26 @@ export class CustomerTypeListComponent implements OnInit {
         this.fillCustomerTypes();
     }
 
+    addType() {
+        var type: CustomerType = new CustomerType();
+        type.id = 0;
+        type.caption = "Новый";
+
+        this.customerTypeService.addCustomerType(type)
+            .then(newType => {
+                this.customerTypes.push(newType);
+            });
+    }
+
+    deleteType(type: CustomerType) {
+        this.customerTypeService.deleteCustomerType(type)
+            .then(res => {
+                if (res) {
+                    this.customerTypes.splice(this.customerTypes.indexOf(type), 1);
+                }
+            });
+    }
+
     private fillCustomerTypes() {
         this.customerTypeService.getCustomerTypes()
             .then(types => this.customerTypes = types);
