ee9c12ad879bf0858e2aed15642f70c8ea7baf27
Koval <KovalPV@corp.lukoil.com>
2016-10-06 13:54:35 +0500

    Реализован список Типов ОГВ

--- sources/mfc.webclient/app/admin/organization-types/organization-type-list/organization-type-list.component.ts
+++ sources/mfc.webclient/app/admin/organization-types/organization-type-list/organization-type-list.component.ts
@@ -9,23 +9,16 @@ import { OrganizationType } from '../organizationType.model';
     templateUrl: 'app/admin/organization-types/organization-type-list/organization-type-list.component.html'
 })
 
-
 export class OrganizationTypeListComponent {
     organizationTypes: OrganizationType[] = [];
     items: string[];
     selectedOrganizationType: OrganizationType;
     
-
     constructor(private organizationTypeService: OrganizationTypeService){
-        //this.items = this.organizationTypeService.getItems();
-        //this.items.push('asd');
         this.refreshList();
     }
 
     private refreshList(): void {
-        this.organizationTypes = this.organizationTypeService.getItems();
-        //this.organizationTypes.push(new OrganizationType(1, 'item 1'));
-        //this.organizationTypes.push(new OrganizationType(2, 'item 2'));
-        //this.organizationTypeService.getOrganizationTypes().then(organization_types_list => this.organizationTypes = organization_types_list);
+        this.organizationTypeService.getOrganizationType().then(orientation_types_list => this.organizationTypes = orientation_types_list);
     }
 }
--- sources/mfc.webclient/app/admin/organization-types/organization-type.service.ts
+++ sources/mfc.webclient/app/admin/organization-types/organization-type.service.ts
@@ -1,25 +1,29 @@
 import { Injectable } from '@angular/core';
-
+import { Http, Response } from '@angular/http';
+import { Observable }     from 'rxjs/Observable';
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/catch'
 import { OrganizationType } from './organizationType.model';
+import { BaseService } from './../../infrastructure/base-service';
 
 @Injectable()
-export class OrganizationTypeService  {
-
-
-    getItems() : OrganizationType[]{
-
-        return [
-            new OrganizationType(1, 'item 0'),
-            new OrganizationType(2, 'item 1'),
-            new OrganizationType(3, 'item 2'),
-            new OrganizationType(4, 'item 4'),
-        ]; 
+export class OrganizationTypeService extends BaseService {
+    constructor(http: Http) {
+        super(http);
     }
 
-/*
-    getItems() : string[] {
-        
-        return ['item 0', 'item 1'];
+    getOrganizationType() : Promise<OrganizationType[]>{
+        return this._http.get(this.apiUrl + 'organizationTypeApi')
+            .toPromise()
+            .then(this.extractData)
+            .catch(this.handlerError);
     }
-*/
+
+    private extractData(res: Response) {
+        return res.json();
+    }
+
+    private handlerError(error: any) {
+        console.log(error.message);
+    }
 }
