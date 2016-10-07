ac74e00306588bff86b90e165081a0e959f32311
YusupovZamir <jusupovz@gmail.com>
2016-10-07 23:49:19 +0500

    исправлены некоторые костыльные решения при работе с пользователями

--- sources/mfc.webclient/app/admin/users/user-list/user-list.component.ts
+++ sources/mfc.webclient/app/admin/users/user-list/user-list.component.ts
@@ -24,15 +24,9 @@ export class UserListComponent {
     }
 
     selectUser(index: number) {
-        let find_user = this.users[index];
-        let user = new User();
         this.selectedUserIndex = index;
-        user.id = find_user.id;
-        user.user_name = find_user.user_name;
-        user.description = find_user.description;
-        user.is_admin = find_user.is_admin;
-        user.is_expert = find_user.is_expert;
-        user.is_controller = find_user.is_controller;
+        let user = new User();
+        Object.assign(user,this.users[index]);
         this.selectedUser = user;
     }
 
@@ -81,11 +75,7 @@ export class UserListComponent {
     }
 
     private refreshUser(index: number, user: User) {
-        this.users[index].user_name = user.user_name;
-        this.users[index].description = user.description;
-        this.users[index].is_admin = user.is_admin;
-        this.users[index].is_controller = user.is_controller;
-        this.users[index].is_expert = user.is_expert;
+        Object.assign(this.users[index],user);
     }
 
     private refreshUserList(): void {
