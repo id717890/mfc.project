fb87160fb0d41c0c1f69435b9a161c8afdc9e607
Semeneev Eldar <cemeheeb@gmail.com>
2016-10-08 16:09:29 +0500

    backend Реализован контроллер для управлением справочником "Виды деятельности"

---
+++ sources/mfc.webapi/Models/ActionTypeInfo.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace mfc.webapi.Models
+{
+    public class ActionTypeInfo
+    {
+        public ActionTypeInfo() { }
+
+        public ActionTypeInfo(domain.entities.ActionType fileStatus)
+        {
+            Id = fileStatus.Id;
+            Caption = fileStatus.Caption;
+            NeedMakeFile = fileStatus.NeedMakeFile;
+        }
+
+        [Display(Name = "Идентификатор")]
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [Required]
+        [Display(Name = "Название")]
+        [JsonProperty("caption")]
+        public string Caption { get; set; }
+
+        [Required]
+        [Display(Name = "Создавать дела")]
+        [JsonProperty("need_make_file")]
+        public bool NeedMakeFile { get; set; }
+
+        public domain.entities.ActionType ConvertToFileStatus()
+        {
+            return new domain.entities.ActionType
+            {
+                Id = this.Id,
+                Caption = this.Caption,
+                NeedMakeFile = this.NeedMakeFile
+            };
+        }
+    }
+}
