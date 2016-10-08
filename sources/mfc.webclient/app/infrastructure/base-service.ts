9df05d17f3de9271da3aff50713703be6d2e713c
Semeneev Eldar <cemeheeb@gmail.com>
2016-10-08 16:10:35 +0500

    Добавлена функция формирующая application/json заголовок

--- sources/mfc.webclient/app/infrastructure/base-service.ts
+++ sources/mfc.webclient/app/infrastructure/base-service.ts
@@ -1,9 +1,14 @@
 ﻿import { Injectable } from '@angular/core';
-import { Http, Headers, Response } from '@angular/http';
+import { Http, Headers, RequestOptions, RequestOptionsArgs, Response } from '@angular/http';
 
 @Injectable()
 export class BaseService  {
     protected apiUrl: string = "http://localhost:4664/api/";
     constructor(protected _http: Http) {
+    }
+
+    protected optionsDefaults() : RequestOptionsArgs {
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        return new RequestOptions({ headers });
     }
 }
